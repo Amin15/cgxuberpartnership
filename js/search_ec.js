1a4da3f86724b9ed5bddfc898c7b5bce613471e0
Amin Bashi <aghafele@uwaterloo.ca>
2020-04-05 15:36:52 -0400

    ab

--- js/search_ec.js
+++ js/search_ec.js
@@ -1,6 +1,6 @@
-function searchFunction2(){
+function searchFunction3(){
     var q3 = document.getElementsByName("query3")[1].value;
     var action_src = "https://eldercare.com/elder-care-provider-jobs/usa?q=" + q3;
     var your_form = document.getElementById('search3');
-    your_form.action = window.open(action_src) ;
+    your_form.action = window.open(action_src);
 }
