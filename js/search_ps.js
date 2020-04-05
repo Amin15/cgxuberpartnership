3cf68d274173177986c82a86107423b907a10154
Amin Bashi <aghafele@uwaterloo.ca>
2020-04-05 04:21:37 -0400

    ab

--- js/search_ps.js
+++ js/search_ps.js
@@ -1,5 +1,3 @@
 function searchFunction(){
     var action_src = window.open("https://petsitter.com/pet-sitter-jobs?q=" + document.getElementsByName("query")[0].value);
-    var your_form = document.getElementById('search');
-    your_form.action = action_src ;
 }
