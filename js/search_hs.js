ef211107e61dac0c336cd4f8572f53e5fcd81881
Amin Bashi <aghafele@uwaterloo.ca>
2020-04-05 19:32:36 -0400

    ab

--- js/search_hs.js
+++ js/search_hs.js
@@ -1,5 +1,5 @@
 function searchFunction4(){
-    var q4 = document.getElementsByName("query3")[1].value;
+    var q4 = document.getElementsByName("query4")[1].value;
     var action_src = "https://housesitter.com/house-sitter-jobs/usa?q=" + q4;
     var your_form = document.getElementById('search4');
     your_form.action = window.open(action_src) ;
