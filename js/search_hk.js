82d19f38fd021a1526a88ab473f7c7f101109ba8
Amin Bashi <aghafele@uwaterloo.ca>
2020-04-05 05:18:25 -0400

    ab

--- js/search_hk.js
+++ js/search_hk.js
@@ -1,5 +1,5 @@
 function searchFunction2(){
-    var q2 = $('query2').val();
+    var q2 = document.getElementsByName("query2")[1].value;
     var action_src = "https://housekeeper.com/housekeeper-jobs?q=" + q2;
     var your_form = document.getElementById('search2');
     your_form.action = window.open(action_src) ;
