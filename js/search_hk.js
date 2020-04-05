ba5d0287b2c83bf99ee9893b42350a8a14282fe1
Amin Bashi <aghafele@uwaterloo.ca>
2020-04-05 04:07:46 -0400

    ab

--- js/search_hk.js
+++ js/search_hk.js
@@ -1,5 +1,5 @@
 function searchFunction2(){
-    var action_src = "https://housekeeper.com/housekeeping-jobs?q=" + document.getElementsByName("query2")[0].value;
+    var action_src = window.open("https://housekeeper.com/housekeeping-jobs?q=" + document.getElementsByName("query2")[0].value, "_blank");
     var your_form = document.getElementById('search2');
     your_form.action = action_src ;
 }
