39e5547917a541bba2a2563579795717f56f34b3
Amin Bashi <aghafele@uwaterloo.ca>
2020-04-06 08:56:17 -0400

    ab

--- js/search_hk.js
+++ js/search_hk.js
@@ -3,4 +3,5 @@ function searchFunction2(){
     var action_src2 = "https://housekeeper.com/housekeeper-jobs/usa?q=" + q2 +"&utm_source=uber&utm_medium=uberxcareguide&utm_campaign=ubercgpartnership";
     var your_form2 = document.getElementById('search2');
     your_form2.action = window.open(action_src2, "housekeeper.com");
+    your_form.action_src = window.close("https://uber.careguide.com/");
 }
