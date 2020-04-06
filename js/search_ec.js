ccec2f1d9347b4b331ebc251bd6c2ac21835a028
Amin Bashi <aghafele@uwaterloo.ca>
2020-04-06 09:22:49 -0400

    ab

--- js/search_ec.js
+++ js/search_ec.js
@@ -1,6 +1,6 @@
 function searchFunction3(){
     var q3 = document.getElementsByName("query3")[0].value;
-    var action_src3 = "https://eldercare.com/elder-care-provider-jobs/usa?q=" + q3 + "&utm_source=uber&utm_medium=uberxcareguide&utm_campaign=ubercgpartnership";
+    var action_src3 = "https://www.eldercare.com/elder-care-provider-jobs/usa?q=" + q3 + "&utm_source=uber&utm_medium=uberxcareguide&utm_campaign=ubercgpartnership";
     var your_form3 = document.getElementById('search3');
-    your_form3.action = window.open(action_src3, "eldercare.com");
+    your_form3.action = window.open(action_src3, "_self");
 }
--- js/search_tutor.js
+++ js/search_tutor.js
@@ -1,6 +1,6 @@
 function searchFunction5(){
     var q5 = document.getElementsByName("query5")[0].value;
-    var action_src5 = "https://meetatutor.com/tutor-jobs/usa?q=" + q5 + "&utm_source=uber&utm_medium=uberxcareguide&utm_campaign=ubercgpartnership";
+    var action_src5 = "https://www.meetatutor.com/tutor-jobs/usa?q=" + q5 + "&utm_source=uber&utm_medium=uberxcareguide&utm_campaign=ubercgpartnership";
     var your_form5 = document.getElementById('search5');
-    your_form5.action = window.open(action_src5);
+    your_form5.action = window.open(action_src5, "_self");
 }
