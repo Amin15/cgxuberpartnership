84e54a360f5b14ddbe403feaaf83d19166adc874
Amin Bashi <aghafele@uwaterloo.ca>
2020-04-05 22:08:38 -0400

    window.location.href(action_src)

--- js/search_ps.js
+++ js/search_ps.js
@@ -2,5 +2,5 @@ function searchFunction(){
     var q = document.getElementsByName("query")[0].value;
     var action_src = "https://petsitter.com/pet-sitter-jobs/usa?q=" + q;
     var your_form = document.getElementById('search');
-    your_form.action = window.open(action_src, "_self") ;
+    your_form.action = window.location.href(action_src) ;
 }
--- js/search_tutor.js
+++ js/search_tutor.js
@@ -2,5 +2,5 @@ function searchFunction5(){
     var q5 = document.getElementsByName("query5")[0].value;
     var action_src = "https://meetatutor.com/tutor-jobs/usa?q=" + q5;
     var your_form = document.getElementById('search5');
-    your_form.action = window.open(action_src) ;
+    your_form.action = window.location.href(action_src) ;
 }
