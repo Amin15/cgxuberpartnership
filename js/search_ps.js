5ecc8ac4d1d677c8e8adfac50222b750c94a09c5
Amin Bashi <aghafele@uwaterloo.ca>
2020-04-06 09:12:20 -0400

    ab

--- js/search_ps.js
+++ js/search_ps.js
@@ -3,5 +3,5 @@ function searchFunction(){
     var action_src = "https://petsitter.com/pet-sitter-jobs/usa?q=" + q + "&utm_source=uber&utm_medium=uberxcareguide&utm_campaign=ubercgpartnership";
     var your_form = document.getElementById('search');
     your_form.action = window.open(action_src, "petsitter.com");
-    your_form.action_src = window.close("https://uber.careguide.com/[object%20Window]");
+    your_form.action = window.close("https://uber.careguide.com/[object%20Window]");
 }
