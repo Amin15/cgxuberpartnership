d691886b3e4cf0cdc5bf85f641c467a74edbd8c3
Amin Bashi <aghafele@uwaterloo.ca>
2020-04-05 22:44:36 -0400

    ab

--- js/search_tutor.js
+++ js/search_tutor.js
@@ -1,6 +1,6 @@
 function searchFunction5(){
     var q5 = document.getElementsByName("query5")[0].value;
-    var action_src = "https://meetatutor.com/tutor-jobs/usa?q="+q5;
+    var action_src = "https://meetatutor.com/tutor-jobs/usa?q=" + q5 + "&utm_source=uber&utm_medium=uberxcareguide&utm_campaign=ubercgpartnership";
     var your_form = document.getElementById('search5');
-    your_form.action = window.open(action_src, "_self");
+    your_form.action = window.open(action_src);
 }
