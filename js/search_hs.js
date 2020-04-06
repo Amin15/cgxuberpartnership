989d10e522bfdab1f6ddf5d75b5efa8979d353bd
Amin Bashi <aghafele@uwaterloo.ca>
2020-04-05 22:16:20 -0400

    spacing

--- js/search_hs.js
+++ js/search_hs.js
@@ -1,6 +1,6 @@
 function searchFunction4(){
     var q4 = document.getElementsByName("query4")[0].value;
-    var action_src = "https://housesitter.com/house-sitter-jobs/usa?q=" + q4;
+    var action_src = "https://housesitter.com/house-sitter-jobs/usa?q=" + q4 + "&utm_source=uber&utm_medium=uberxcareguide&utm_campaign=ubercgpartnership";
     var your_form = document.getElementById('search4');
-    your_form.action = window.location.href(action_src) ;
+    your_form.action = window.location.href(action_src);
 }
